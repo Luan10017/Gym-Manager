4b2c5874d72b01c2886cbe649c30fa39543e2420
Luan Andrade <luan10017@hotmail.com>
2020-05-22 17:31:48 -0300

    Adicionada tela de detalhes dos instrutores e estruturas de manipulação informações back to front end

--- instructors.js
+++ instructors.js
@@ -1,7 +1,27 @@
-// create
 const fs = require('fs')
 const data = require('./data.json')
+const { age } = require('./utils')
 
+//show
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+    if (!foundInstructor) return res.send('Instructor not found')
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(','),
+        created_at: "",
+    }
+
+    return res.render('instructors/show', {instructor})
+}
+
+// create
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
     for (key of keys) {
