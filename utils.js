c66ca59bf0537f5cf4dfe0f140b300d73ec80187
Luan Andrade <luan10017@hotmail.com>
2020-05-24 17:23:26 -0300

    Adicionada estrutura de include.njk para reaproveitamento de códico entre páginas creat e edit. Além de formatação de datas para retorno no frontend

--- utils.js
+++ utils.js
@@ -10,5 +10,20 @@ module.exports = {
             age = age -1
         }
         return age
+    },
+    date: function(timestamp) {
+        const date = new Date(timestamp)
+        
+        //yyyy
+        const year = date.getUTCFullYear()
+        // mm
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+
+        //dd    
+        const day = `0${date.getUTCDate()}`.slice(-2)
+        
+        return `${year}-${month}-${day}`
+        
+
     }
 }
