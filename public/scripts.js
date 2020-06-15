70173de8226aa6ee8c02415de04c3bbb49b79a85
Luan Andrade <luan10017@hotmail.com>
2020-06-15 15:05:34 -0300

    Finalizada lógica de destaque do header

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,7 @@ const currentPage = location.pathname
 const menuItens = document.querySelectorAll("header .links a")
 
 for (item of menuItens) {
-    if(currentPage == item.getAttribut("href")){
+    if(currentPage.includes(item.getAttribute("href"))) {
         item.classList.add("active")
     }
 }
